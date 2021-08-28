f42a940cfa4385f653e7836a913277e3c68aa2ba
mukesh <mukeshyadav1105@gmail.com>
2021-08-28 13:21:35 -0500

    initial commit

---
+++ busywait43.c
@@ -0,0 +1,71 @@
+#include <stdio.h>                                        
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include<time.h>
+long thread_count;
+long long n=40000;
+double a=0;
+double b=2;
+long double h;
+long long trap_count;
+int flag;
+
+long double sum;
+void* sump(void* rank);
+int main(int argc, char* argv[])
+{
+    long thread;
+    thread_count = atoll(argv[1]);
+    pthread_t* thread_handles;
+    
+    flag=0;
+    
+
+    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
+    sum = 0.0;
+    h = (long double)(b-a)/n;
+    trap_count = n/thread_count;
+
+    clock_t begin = clock();
+	printf("hello from main thread");
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_create(&thread_handles[thread], NULL, sump, (void*)thread);
+
+	
+
+
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_join(thread_handles[thread], NULL);
+    clock_t end = clock();
+    double elapsed = (double)(end - begin)/CLOCKS_PER_SEC;
+
+    sum = h*sum;
+    printf("n = %lld\n", n);
+    printf("   Our estimate of area = %f\n", (double)sum);
+    printf("The elapsed time is %f seconds\n", elapsed);
+    free(thread_handles);
+
+    return 0;
+}
+
+void* sump(void* rank) {
+    
+long my_rank = (long) rank;
+    long double x;
+    long double my_sum = 0.0;
+    long double start = a+trap_count*my_rank*h;
+    long double end = a+trap_count*(my_rank+1)*h;
+
+    my_sum = (start*start +end*end)/2.0;
+    for (long long i = 1; i <= trap_count; i++)
+    {
+        x = start+i*h;
+        my_sum +=x*x;
+    }
+while (flag != my_rank);
+sum += my_sum;
+flag = (flag+1) % thread_count;
+
+return NULL;
+}                                                                                                                                                                                                                                                                                                                    
---
+++ mutex43.c
@@ -0,0 +1,72 @@
+#include <stdio.h>                                        
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include<time.h>
+long thread_count;
+long long n=40000;
+double a=0;
+double b=2;
+long double h;
+long long trap_count;
+pthread_mutex_t mutex;
+
+long double sum;
+void* sump(void* rank);
+int main(int argc, char* argv[])
+{
+    long thread;
+    thread_count = atoll(argv[1]);
+    pthread_t* thread_handles;
+    
+    pthread_mutex_init(&mutex, NULL);
+    
+
+    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
+    sum = 0.0;
+    h = (long double)(b-a)/n;
+    trap_count = n/thread_count;
+
+    clock_t begin = clock();
+	printf("hello from main thread");
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_create(&thread_handles[thread], NULL, sump, (void*)thread);
+
+	
+
+
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_join(thread_handles[thread], NULL);
+    clock_t end = clock();
+    double elapsed = (double)(end - begin)/CLOCKS_PER_SEC;
+
+    sum = h*sum;
+    printf("n = %lld\n", n);
+    printf("   Our estimate of area = %f\n", (double)sum);
+    printf("The elapsed time is %f seconds\n", elapsed);
+    free(thread_handles);
+
+    return 0;
+}
+
+void* sump(void* rank) {
+    
+long my_rank = (long) rank;
+    long double x;
+    long double my_sum = 0.0;
+    long double start = a+trap_count*my_rank*h;
+    long double end = a+trap_count*(my_rank+1)*h;
+
+    my_sum = (start*start +end*end)/2.0;
+    for (long long i = 1; i <= trap_count; i++)
+    {
+        x = start+i*h;
+        my_sum +=x*x;
+    }
+
+    pthread_mutex_lock(&mutex);
+    sum += my_sum;
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+}                                                                                                                                                                                                                                                                                                                    
